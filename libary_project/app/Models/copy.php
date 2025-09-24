e133fcbbb4fea132a335257af0fc7c185bd59a9e
Horváth Dzsenifer Izolda (Szf_F_2024_2026) <horvath.dzsenifer.izolda1@diak.szamalk-szalezi.hu>
2025-09-24 18:13:52 +0200

    elso

---
+++ libary_project/app/Models/copy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class copy extends Model
+{
+    protected $fillable = [
+        'book_id',
+        'user_id'
+    ];
+}
